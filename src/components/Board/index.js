96b6c2111701e204620163eac8ab3d1a27d7af79
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-08 16:52:52 -0500

    Refactored + winPieces to X on cell

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -21,7 +21,8 @@ class Board extends Component {
                 ['x', 'x', 'x', 'x', 'x', 'x'],
                 ['x', 'x', 'x', 'x', 'x', 'x'],
                 ['x', 'x', 'x', 'x', 'x', 'x'],
-                ['x', 'x', 'x', 'x', 'x', 'x']]
+                ['x', 'x', 'x', 'x', 'x', 'x']],
+            winningPieces: []
         };
     }
 
@@ -37,70 +38,91 @@ class Board extends Component {
                 ['x', 'x', 'x', 'x', 'x', 'x'],
                 ['x', 'x', 'x', 'x', 'x', 'x'],
                 ['x', 'x', 'x', 'x', 'x', 'x'],
-                ['x', 'x', 'x', 'x', 'x', 'x']]
+                ['x', 'x', 'x', 'x', 'x', 'x']],
+            winningPieces: []
         });
     }
 
-    changePlayer = () => {
+    changePlayer = (player) => {
         let newPlayer = 'r';
-        if (this.state.player === 'r') newPlayer = 'y';
-        this.setState({
-            player: newPlayer,
-            gameStateLabel: `It is ${players[newPlayer]}'s turn`
-        });
+        if (player === 'r') newPlayer = 'y';
+        return newPlayer;
     }
 
-    makeMove = (colIndex) => {
+    canPlay = (board, colIndex) => {
+        if (colIndex === null || colIndex < 0 || colIndex > 6) return false;
+        let heightIndex = board[colIndex].findIndex(el => el === 'x');
+        return heightIndex !== -1;
+    }
+
+    play = (board, player, colIndex) => {
+        let heightIndex = board[colIndex].findIndex(el => el === 'x');
+        board[colIndex][heightIndex] = player;
+    }
+
+    makeMove = colIndex => {
         const { board, player } = this.state;
         // Check if move is valid
-        if (colIndex === null || colIndex < 0 || colIndex > 6) return;
-        let heightIndex = board[colIndex].findIndex(el => el !== 'r' && el !== 'y');
-        if (heightIndex === -1) return;
-
+        if (!this.canPlay(board, colIndex)) return;
         console.log(`${players[player]} player on col ${colIndex}`);
-
         //Deep copy array to avoid modifying state directly
         let newBoard = JSON.parse(JSON.stringify(board));
         // Update board state with move made
-        newBoard[colIndex][heightIndex] = player;
+        this.play(newBoard, player, colIndex);
         // Checks state of game after the move made
-        this.setState({ board: newBoard }, this.gameStateCheck);
+        this.setState({ board: newBoard });
+        this.gameStateCheck(newBoard, player);
     }
 
-    gameStateCheck = () => {
-        const { player } = this.state;
+    gameStateCheck = (board, player) => {
         // Checks for win condition or tie, else game continues
-        if (this.checkWin()) {
-            this.setState({ gameStateLabel: `${players[player]} won!`, gameActive: false });
-        } else if (this.boardIsFull()) {
+        let winPieces = this.checkWin(board, player);
+        if (winPieces.length >= 4) {
+            this.setState({ gameStateLabel: `${players[player]} won!`, gameActive: false, winningPieces: winPieces });
+        } else if (this.boardIsFull(board)) {
             this.setState({ gameStateLabel: `It was a tie!`, gameActive: false });
         }
         else {
-            this.changePlayer();
+            let newPlayer = this.changePlayer(player);
+            this.setState({
+                player: newPlayer,
+                gameStateLabel: `It is ${players[newPlayer]}'s turn`
+            });
         }
     }
 
-    checkWin = () => {
-        let { board, player } = this.state;
+    checkWin = (board, player) => {
+        if (!board || !player) return [];
         // Check verticals
-        for (const col of board) {
-            if (col[2] === col[3] && col[3] === player) {
-                if ((col[0] === player && col[1] === player)
-                    || (col[1] === player && col[4] === player)
-                    || (col[4] === player && col[5] === player)
-                ) return true;
+        for (let col = 0; col < board.length; col++) {
+            for (let row = 0; row < 4; row++) {
+                if (board[col][row] === player
+                    && board[col][row + 1] === player
+                    && board[col][row + 2] === player
+                    && board[col][row + 3] === player
+                ) return [
+                    this.coordID(col, row),
+                    this.coordID(col, row + 1),
+                    this.coordID(col, row + 2),
+                    this.coordID(col, row + 3)
+                ];
             }
         }
 
         // Check horizontals
-        for (const row in board) {
+        for (let row = 0; row < board[0].length; row++) {
             if (board[3][row] === player) {
                 for (let col = 0; col < 4; col++) {
                     if (board[col][row] === player
                         && board[col + 1][row] === player
                         && board[col + 2][row] === player
                         && board[col + 3][row] === player
-                    ) return true;
+                    ) return [
+                        this.coordID(col, row),
+                        this.coordID(col + 1, row),
+                        this.coordID(col + 2, row),
+                        this.coordID(col + 3, row)
+                    ];
                 }
             }
         }
@@ -114,22 +136,31 @@ class Board extends Component {
                     board[col + 1][offset + 1] === player &&
                     board[col + 2][offset + 2] === player &&
                     board[col + 3][offset + 3] === player
-                ) return true;
+                ) return [
+                    this.coordID(col, offset),
+                    this.coordID(col + 1, offset + 1),
+                    this.coordID(col + 2, offset + 2),
+                    this.coordID(col + 3, offset + 3)
+                ];
 
                 // Downwards
                 if (board[col][boardHeight - offset] === player &&
                     board[col + 1][boardHeight - (offset + 1)] === player &&
                     board[col + 2][boardHeight - (offset + 2)] === player &&
                     board[col + 3][boardHeight - (offset + 3)] === player
-                ) return true;
+                ) return [
+                    this.coordID(col, boardHeight - offset),
+                    this.coordID(col + 1, boardHeight - (offset + 1)),
+                    this.coordID(col + 2, boardHeight - (offset + 2)),
+                    this.coordID(col + 3, boardHeight - (offset + 3))
+                ];
             }
         }
 
-        return false;
+        return [];
     }
 
-    boardIsFull = () => {
-        let { board } = this.state;
+    boardIsFull = (board) => {
         for (const col of board) {
             if (col[col.length - 1] === 'x') {
                 return false;
@@ -138,11 +169,15 @@ class Board extends Component {
         return true;
     }
 
+    coordID = (col, row) => {
+        return col * 7 + row
+    }
 
-    displayCell = (cell, i) => {
-        if (cell === 'r') return <div className='circle red' colindex={i} />
-        else if (cell === 'y') return <div className='circle yellow' colindex={i} />
-        return <div className='circle empty' colindex={i} />;
+    displayCell = (cell, col, row) => {
+        let x = this.state.winningPieces.includes(this.coordID(col, row)) ? <div className='x' colindex={col} /> : '';
+        if (cell === 'r') return <div className='circle red' colindex={col}>{x}</div>
+        else if (cell === 'y') return <div className='circle yellow' colindex={col} >{x}</div>
+        return <div className='circle empty' colindex={col} />;
     }
 
     colClicked = e => {
@@ -161,7 +196,7 @@ class Board extends Component {
                     {board.map((col, i) => (
                         <div className='board-col' key={i} colindex={i} onClick={this.colClicked}>
                             {col.map((cell, j) => (
-                                <div className='board-cell' key={j} colindex={i}>{this.displayCell(cell, i)}</div>
+                                <div className='board-cell' key={j} colindex={i}>{this.displayCell(cell, i, j)}</div>
                             ))}
                         </div>
                     ))}

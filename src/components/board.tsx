a819c10513536cb9a095ce4c793b27185ac267cd
JorgeGiovannetti <giovannetti2000@gmail.com>
2022-10-19 13:39:08 -0500

    Board representation of position key

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,87 +1,43 @@
-import { ReactNode, useEffect, useState } from "react";
-import Position from "../game/Position";
+import { ReactNode } from "react";
 
 interface BoardProps {
-  position: Position;
-  playCol: (col: bigint) => void;
+  readonly positionKey: bigint;
+  readonly makeMove: (col: bigint) => void;
 }
 
-const getBoardRepresentation = (
-  boardState: bigint,
-  representationName: string
-): ReactNode => {
-  const boardStringRepresentation: ReactNode[] = [];
+const Board: React.FC<BoardProps> = ({ positionKey, makeMove }) => {
+  const boardRepresentation: ReactNode[] = [];
 
-  const getPositionCol = (col: bigint): string => {
-    const n = (boardState >> (col * BigInt(7))) & BigInt(0x3f);
-    const rowStr = n.toString(2).padStart(7, "0");
+  const getPositionCol = (col: bigint): ReactNode => {
+    const n = (positionKey >> (col * BigInt(7))) & BigInt(0x3f);
+    const colArr = n.toString(2).padStart(7, "0").split("");
 
-    return rowStr;
+    return (
+      <div className="container flex flex-col px-4">
+        {colArr.map((cell, index) => (
+          <div key={index} className="text-xl font-semibold">
+            {cell}
+          </div>
+        ))}
+      </div>
+    );
   };
 
-  for (let col = 0; col < 7; col++) {
-    boardStringRepresentation.push(
+  for (let col = 0n; col < 7n; col++) {
+    boardRepresentation.push(
       <div
-        key={col}
-        style={{ writingMode: "vertical-lr", textOrientation: "upright" }}
+        key={Number(col)}
+        onClick={() => makeMove(col)}
+        className="select-none hover:bg-blue-400"
       >
-        {getPositionCol(BigInt(col))}
+        {getPositionCol(col)}
       </div>
     );
   }
 
   return (
-    <div>
-      <div>{representationName}</div>
-      <div className="columns-7 bg-cyan-400 font-mono">
-        {boardStringRepresentation}
-      </div>
-    </div>
-  );
-};
-
-const Board: React.FC<BoardProps> = ({ position, playCol }) => {
-  console.log("Current position", position);
-
-  const [positionRepresentation, setPositionRepresentation] = useState(
-    getBoardRepresentation(position.current_position, "currentPosition")
-  );
-  const [maskRepresentation, setMaskRepresentation] = useState(
-    getBoardRepresentation(position.mask, "mask")
-  );
-  const [keyRepresentation, setKeyRepresentation] = useState(
-    getBoardRepresentation(position.key(), "key")
-  );
-
-  return (
-    <div className="m-4">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const colPlayed = e.target.move.value as number;
-          playCol(BigInt(colPlayed));
-          setPositionRepresentation(
-            getBoardRepresentation(position.current_position, "currentPosition")
-          );
-          setMaskRepresentation(getBoardRepresentation(position.mask, "mask"));
-          setKeyRepresentation(getBoardRepresentation(position.key(), "key"));
-        }}
-      >
-        <input
-          name="move"
-          type="number"
-          min={0}
-          max={6}
-          defaultValue={0}
-          className="mr-4 bg-gray-200 py-2 px-4"
-        ></input>
-        <button type="submit" className="bg-purple-300 py-2 px-4">
-          play move
-        </button>
-      </form>
-      {positionRepresentation}
-      {maskRepresentation}
-      {keyRepresentation}
+    <div className="flex columns-7 bg-blue-500 font-mono">
+      {boardRepresentation}
     </div>
   );
 };
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,30 +1,52 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
-import Board from "../components/board";
+import { ReactNode, useState } from "react";
+import Board from "../components/Board";
 import Position from "../game/Position";
 
 const Play: NextPage = () => {
   const [position, setPosition] = useState<Position>(new Position());
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [positionKey, setPositionKey] = useState(0n);
+  const [isTied, setIsTied] = useState(false);
+
+  const resetGame = () => {
+    setPosition(new Position());
+    setIsGameOver(false);
+    setPositionKey(0n);
+    setIsTied(false);
+  };
 
   const playCol = (col: bigint) => {
-    console.log("Position before playing col " + col + ":", position);
-    if (position.canPlay(col)) {
-      if (position.isWinningMove(col)) {
-        console.log(
-          "%c" + "GG u win",
-          "color:" + "#00ff00" + ";font-weight:bold;"
-        );
-      } else {
-        console.warn("Not a winner");
-      }
-      position.playCol(col);
-      console.log("Played col", col);
-      console.log("Position now", position);
-    } else {
-      console.error("Tried to play invalid col");
+    if (isGameOver || !position.canPlay(col)) {
+      return;
     }
+
+    if (position.isWinningMove(col)) {
+      setIsGameOver(true);
+    } else if (position.nbMoves() + 1n >= Position.WIDTH * Position.HEIGHT) {
+      setIsTied(true);
+      setIsGameOver(true);
+    }
+
+    position.playCol(col);
+    setPositionKey(position.key());
+  };
+
+  const displayGameOver = (): ReactNode => {
+    if (isTied) {
+      return (
+        <div className="text-3xl font-extrabold leading-normal text-gray-700 md:text-[2rem]">
+          It was a tie!
+        </div>
+      );
+    }
+    return (
+      <div className="text-3xl font-extrabold leading-normal text-gray-700 md:text-[2rem]">
+        Player {Number(position.nbMoves() + 1n) % 2} has won!
+      </div>
+    );
   };
 
   return (
@@ -34,12 +56,22 @@ const Play: NextPage = () => {
         <meta name="description" content="Connect 4" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>Play!</div>
-        <Link href="/">
-          <button>Back to main menu</button>
-        </Link>
-        <Board position={position} playCol={playCol} />
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+        <div className="mb-4 flex space-x-4">
+          <Link href="/">
+            <button className="rounded-md bg-red-400 py-2 px-4 hover:bg-red-300">
+              Back to menu
+            </button>
+          </Link>
+          <button
+            className="rounded-md bg-purple-400 py-2 px-4 hover:bg-purple-300"
+            onClick={resetGame}
+          >
+            Restart
+          </button>
+        </div>
+        {isGameOver ? displayGameOver() : null}
+        <Board positionKey={positionKey} makeMove={playCol} />
       </main>
     </>
   );
